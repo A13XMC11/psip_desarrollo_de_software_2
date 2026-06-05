19cf8b62970f0ed9efae427fa1e30a4e364c3231
Alexander Mejia <99159842+A13XMC11@users.noreply.github.com>
2026-06-05 11:47:36 -0500

    update: esquema de proyecto

---
+++ asistencia/src/test/java/com/uisrael/asistencia/AsistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uisrael.asistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
